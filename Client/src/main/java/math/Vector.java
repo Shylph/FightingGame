1d5269e7b0e05d83e86b25ed22c0e1f17b32ecb9
sylphe <myks789@gmail.com>
2015-04-27 21:41:00 +0900

    add math Vector file

---
+++ Client/src/main/java/math/Vector.java
@@ -0,0 +1,41 @@
+package math;
+
+import static java.lang.Math.sqrt;
+
+/**
+ * Created by Kang SangHun on 2015-04-27.
+ */
+public class Vector {
+    public double x, y;
+
+    public Vector() {
+    }
+
+
+    public Vector(double _x, double _y) {
+        x = _x;
+        y = _y;
+    }
+
+    public double length() {
+        return sqrt(x * x + y * y);
+    }
+
+    public void normalize(){
+        double l = length();
+        x = x/l;
+        y = y/l;
+    }
+    public void add(Vector v){
+        x+=v.x;
+        y+=v.y;
+    }
+
+    public static Vector add(Vector v1,Vector v2){
+        return new Vector(v1.x+v2.x,v1.y+v2.y);
+    }
+
+    public static double dot(Vector a, Vector b){
+        return a.x*b.x+a.y*b.y;
+    }
+}
